1a8bfb68f466c426a7614783134303d751720fb6
Nichollas <nichollas.tenorio.sj@gmail.com>
2026-08-25 17:41:31 -0300

    questao 2 feita

--- pages/question_2.php
+++ pages/question_2.php
@@ -33,27 +33,27 @@
         <div class="question-number">2</div>
 
         <div class="quiz-image">
-            <img src="../assets/images/" alt="Imagem do quiz">
+            <img src="../assets/images/questao_2.png" alt="Imagem do quiz">
         </div>
 
         <div class="question">
-            Questão
+            Carlos encontrou uma garrafa contendo CH₃CH₂OH no laboratório. O que ele encontrou?
         </div>
         <form action="../core/action.php" method="post">
             <div class="answers">
                 <div class="row">
                     <input type="hidden" name="question_id" value="<?= $pag ?>">
                     <div class="col-6">
-                        <button type="submit" class="answer-button" name="correct"></button>
+                        <button type="submit" class="answer-button">Uma base</button>
                     </div>
                     <div class="col-6">
-                        <button type="submit" class="answer-button"></button>
+                        <button type="submit" class="answer-button">Uma poção de mana</button>
                     </div>
                     <div class="col-6">
-                        <button type="submit" class="answer-button"></button>
+                        <button type="submit" class="answer-button">51 disfarçada</button>
                     </div>
                     <div class="col-6">
-                        <button type="submit" class="answer-button"></button>
+                        <button type="submit" class="answer-button" name="correct">Etanol, um álcool</button>
                     </div>
                 </div>
             </div>
