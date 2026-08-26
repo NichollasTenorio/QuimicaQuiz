6d28d4d632a7135185516b69ca3b9d6468e6127c
MiguelSouza-1 <miguelssouzacontato@gmail.com>
2026-08-26 19:38:30 -0300

    correção da questão 12

--- pages/question_11.php
+++ pages/question_11.php
@@ -1,16 +1,8 @@
 <?php
- session_start();
-    $pag = 11;
+session_start();
 
-    if(!isset($_SESSION['actual_question'])){
-        header('Location: ../home.php');
-        exit;
-    }
-
-    if($_SESSION['actual_question'] != $pag){
-        header("Location: question_{$_SESSION['actual_question']}.php");
-        exit;
-    }
+$pag = 11;
+$_SESSION['actual_question'] = 11;
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -31,15 +23,16 @@
     <div class="quiz-container">
 
         <div class="question-number">11</div>
-
+         <form action="../core/action.php" method="post">
         <div class="quiz-image">
-            <img src="../assets/images/questao_11.png" alt="Imagem do quiz">
+            <footer class="quiz-footer">          
+            <button type="submit" class="footer-button" name="correct"><img src="../assets/images/questao_11.png"></button>
+            </footer>
         </div>
 
         <div class="question">
             Você estava em casa cheio de fome, você viu uma solução de verde bromocresol e vinagre, quando você as misturou para fazer salada, que cor ficou?
         </div>
-        <form action="../core/action.php" method="post">
             <div class="answers">
                 <div class="row">
                     <input type="hidden" name="question_id" value="<?= $pag ?>">
@@ -50,7 +43,7 @@
                         <button type="submit" class="answer-button answer-blue"></button>
                     </div>
                     <div class="col-6">
-                        <button type="submit" class="answer-button answer-yellow" name="correct"></button>
+                        <button type="submit" class="answer-button answer-brown"></button>
                     </div>
                     <div class="col-6">
                         <button type="submit" class="answer-button answer-samuel"></button>
