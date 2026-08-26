5801f5dfcf267a3a4f9b2c31e27ed547caf84457
MiguelSouza-1 <miguelssouzacontato@gmail.com>
2026-08-26 19:42:48 -0300

    arrumando o session

--- pages/question_11.php
+++ pages/question_11.php
@@ -1,8 +1,16 @@
 <?php
-session_start();
+  session_start();
+    $pag = 11;
 
-$pag = 11;
-$_SESSION['actual_question'] = 11;
+    if(!isset($_SESSION['actual_question'])){
+        header('Location: ../home.php');
+        exit;
+    }
+
+    if($_SESSION['actual_question'] != $pag){
+        header("Location: question_{$_SESSION['actual_question']}.php");
+        exit;
+    }
 ?>
 <!DOCTYPE html>
 <html lang="pt-BR">
