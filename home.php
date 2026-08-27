58ab57aaad8f3b7d7247000778279bcbfcda96b3
Nichollas <nichollas.tenorio.sj@gmail.com>
2026-08-27 19:48:53 -0300

    session_destroy() finish page

--- home.php
+++ home.php
@@ -19,7 +19,7 @@ if($_SERVER['REQUEST_METHOD'] == "POST"){
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Química Quiz - Preview</title>
+    <title>Química Quiz</title>
 
     <link
         href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css"
